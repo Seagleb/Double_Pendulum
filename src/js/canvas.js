deebb25d78df75e7570702b4a005468443334885
William <seagleb@gmail.com>
2020-12-09 15:34:35 -0600

    Fixed Gravity rotation.

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -37,8 +37,8 @@ addEventListener('resize', () => {
 // Objects
 class Pendulum {
   constructor(anchor_x, anchor_y, arm_len1, start_theta1, arm_len2, start_theta2, mass1, mass2) {
-    this.factor = .005
-    this.gravity = 9.8 * this.factor
+    this.time_scale = .01
+    this.gravity = 9.8
 
     this.anchor_x = anchor_x
     this.anchor_y = anchor_y
@@ -46,15 +46,15 @@ class Pendulum {
     this.arm_len2 = arm_len2
     this.theta1 = start_theta1
     this.theta2 = start_theta2
-    this.mass1 = mass1 * this.factor
-    this.mass2 = mass2 * this.factor
+    this.mass1 = mass1
+    this.mass2 = mass2
     this.bob_radius1 = mass1 * .5
     this.bob_radius2 = mass2 * .5
 
-    this.bob_x1 = this.anchor_x + this.arm_len1 * Math.cos(this.theta1)
-    this.bob_y1 = this.anchor_y + this.arm_len1 * Math.sin(this.theta1)
-    this.bob_x2 = this.bob_x1 + this.arm_len2 * Math.cos(this.theta2)
-    this.bob_y2 = this.bob_y1 + this.arm_len2 * Math.sin(this.theta2)
+    this.bob_x1 = this.anchor_x + this.arm_len1 * Math.sin(this.theta1)
+    this.bob_y1 = this.anchor_y + this.arm_len1 * Math.cos(this.theta1)
+    this.bob_x2 = this.bob_x1 + this.arm_len2 * Math.sin(this.theta2)
+    this.bob_y2 = this.bob_y1 + this.arm_len2 * Math.cos(this.theta2)
     this.angular_acceleration1 = 0
     this.angular_velocity1 = 0
     this.angular_acceleration2 = 0
@@ -86,29 +86,29 @@ class Pendulum {
   }
 
   update() {
-    this.bob_x1 = this.anchor_x + this.arm_len1 * Math.cos(this.theta1)
-    this.bob_y1 = this.anchor_y + this.arm_len1 * Math.sin(this.theta1)
+    this.bob_x1 = this.anchor_x + this.arm_len1 * Math.sin(this.theta1)
+    this.bob_y1 = this.anchor_y + this.arm_len1 * Math.cos(this.theta1)
 
-    this.bob_x2 = this.bob_x1 + this.arm_len2 * Math.cos(this.theta2)
-    this.bob_y2 = this.bob_y1 + this.arm_len2 * Math.sin(this.theta2)
+    this.bob_x2 = this.bob_x1 + this.arm_len2 * Math.sin(this.theta2)
+    this.bob_y2 = this.bob_y1 + this.arm_len2 * Math.cos(this.theta2)
 
+    // this.angular_velocity1 *= .998
+    // this.angular_velocity2 *= .998
+    
+    this.angular_acceleration1 = ((-this.gravity * (2 * this.mass1 + this.mass2) * Math.sin(this.theta1)
+    - this.mass2 * this.gravity * Math.sin(this.theta1 - 2 * this.theta2)
+    - 2 * Math.sin(this.theta1 - this.theta2) * this.mass2 
+    * (Math.pow(this.angular_velocity2, 2) * this.arm_len2 + Math.pow(this.angular_velocity1, 2) * this.arm_len1 * Math.cos(this.theta1 - this.theta2)))
+    / (this.arm_len1 * (2 * this.mass1 + this.mass2 - this.mass2 * Math.cos(2 * this.theta1 - 2 * this.theta2)))) * this.time_scale
+    
+    this.angular_acceleration2 = ((2 * Math.sin(this.theta1 - this.theta2) 
+    * (Math.pow(this.angular_velocity1, 2) * this.arm_len1 * (this.mass1 + this.mass2)
+    + this.gravity * (this.mass1 + this.mass2) * Math.cos(this.theta1)
+    + Math.pow(this.angular_velocity2, 2) * this.arm_len2 * this.mass2 * Math.cos(this.theta1 - this.theta2)))
+    / (this.arm_len2 * ((2 * this.mass1 + this.mass2 - this.mass2 * Math.cos(2 * this.theta1 - 2 * this.theta2))))) * this.time_scale
+    
     this.angular_velocity1 += this.angular_acceleration1
     this.angular_velocity2 += this.angular_acceleration2
-    this.angular_velocity1 *= .998
-    this.angular_velocity2 *= .998
-
-    this.angular_acceleration1 = ((-this.gravity * (2 * this.mass1 + this.mass2) * Math.sin(this.theta1)
-                                  - this.mass2 * this.gravity * Math.sin(this.theta1 - 2 * this.theta2)
-                                  - 2 * Math.sin(this.theta1 - this.theta2) * this.mass2 
-                                  * (Math.pow(this.angular_velocity2, 2) * this.arm_len2 + Math.pow(this.angular_velocity1, 2) * this.arm_len1 * Math.cos(this.theta1 - this.theta2)))
-                                  / (this.arm_len1 * (2 * this.mass1 + this.mass2 - this.mass2 * Math.cos(2 * this.theta1 - 2 * this.theta2))))
-
-    this.angular_acceleration2 = ((2 * Math.sin(this.theta1 - this.theta2) 
-                                  * (Math.pow(this.angular_velocity1, 2) * this.arm_len1 * (this.mass1 + this.mass2)
-                                  + this.gravity * (this.mass1 + this.mass2) * Math.cos(this.theta1)
-                                  + Math.pow(this.angular_velocity2, 2) * this.arm_len2 * this.mass2 * Math.cos(this.theta1 - this.theta2)))
-                                  / (this.arm_len2 * ((2 * this.mass1 + this.mass2 - this.mass2 * Math.cos(2 * this.theta1 - 2 * this.theta2)))))
-
     this.theta1 += this.angular_velocity1
     this.theta2 += this.angular_velocity2
 
@@ -119,7 +119,7 @@ class Pendulum {
 // Implementation
 let objects
 function init() {
-  objects = [new Pendulum(canvas.width / 2, canvas.height/2, 100, Math.PI, 100, Math.PI/2, 20, 20)]
+  objects = [new Pendulum(canvas.width / 2, canvas.height / 2, 100, Math.PI/2, 100, Math.PI, 10, 10)]
 
   // for (let i = 0; i < 400; i++) {
   //    objects.push()
